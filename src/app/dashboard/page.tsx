d693d7dbdb74919e9479bf28314f21d83a63b6be
Omar Issa <omarissa@oryxdentalsoftware.com>
2024-04-24 21:50:08 +0200

    synced prisma and trpc

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { get } from "http";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Page = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  // check if user is logged in
+  if (!user || !user.id) {
+    redirect("/auth-callback?origin=dashboard");
+  }
+
+  const dbUser = await db.user.findUnique({
+    where: { id: user.id },
+  });
+
+  // check if user in db
+  if (!dbUser) {
+    redirect("/auth-callback?origin=dashboard");
+  }
+
+  return <div>{user.email}</div>;
+};
+
+export default Page;
